e3b62cfcf065ccb4e9db7f0441f22c2d82e51692
Samuel Rince <s@rince.me>
2022-10-14 11:45:29 +0200

    feat: create first route to upload a benchmark file

--- api/api/routes.py
+++ api/api/routes.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, UploadFile
 
 route = APIRouter(
     prefix='/v1',
 )
+
+
+@route.post('/upload_benchmark')
+async def upload_benchmark(file: UploadFile) -> None:
+    pass
