6f707334df7c0fa141607f33efa5a7ccbf09cfbb
Victorien Molle <biche@biche.re>
2022-10-15 14:06:04 +0200

    sensors: rework base class
     - start method is now implemented in the base class
     - add a 'run_for' method to start and stop a test after a specified duration
     - add a 'load' property of the CPUStressor class to stress the CPU at a specific load
        (100% by default)
     - rework the main function with examples

--- energizta/stressor.py
+++ energizta/stressor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from multiprocessing import cpu_count
-from subprocess import PIPE, Popen
+from subprocess import Popen
 from time import ctime, sleep, time
 from json import dump
 
@@ -12,10 +12,12 @@ class StressorException(Exception):
 
 class Stressor:
     def __init__(self):
+        self._path = '/usr/bin/stress-ng'
+        self._cmdline = ''
         self._process = None
 
     def start(self):
-        raise NotImplementedError()
+        self._process = Popen([self._path] + self._cmdline.split())
 
     def stop(self):
         if not self._process:
@@ -23,11 +25,17 @@ class Stressor:
 
         self._process.kill()
 
+    def run_for(self, sleep_until):
+        self.start()
+        sleep(sleep_until)
+        self.stop()
+
 
 class RAMStressor(Stressor):
     def __init__(self, workers=2):
         super().__init__()
         self._workers_num = workers
+        self._cmdline = f'--vm {self.workers} --vm-bytes 1G'
 
     @property
     def workers(self):
@@ -37,17 +45,17 @@ class RAMStressor(Stressor):
     def name(self):
         return 'stress-ng ram'
 
-    def start(self):
-        prog = '/usr/bin/stress-ng'
-        cmdline = f'--vm {self.workers} --vm-bytes 1G'
-
-        self._process = Popen([prog] + cmdline.split(), stdout=PIPE)
-
 
 class CPUStressor(Stressor):
     def __init__(self):
         super().__init__()
+        self._start = 0
+        self._load = 100
         self._cpu_num = cpu_count()
+        self._cmdline = f'--cpu {self.num_cpu} -l {self.load}'
+
+        if not self.num_cpu:
+            raise StressorException('Could not run stressor on 0 CPU!')
 
     @property
     def num_cpu(self):
@@ -57,14 +65,17 @@ class CPUStressor(Stressor):
     def name(self):
         return 'stress-ng cpu'
 
-    def start(self, load_percent):
-        prog = '/usr/bin/stress-ng'
-        cmdline = f'--cpu {self.num_cpu} -l {load_percent}'
+    @property
+    def load(self):
+        return self._load
 
-        if not self.num_cpu:
-            raise StressorException('Could not run stressor on 0 CPU!')
-
-        self._process = Popen([prog] + cmdline.split(), stdout=PIPE)
+    @load.setter
+    def load(self, load):
+        if 0 < load <= 100:
+            self._load = load
+            self._cmdline = f'--cpu {self.num_cpu} -l {self.load}'
+        else:
+            raise StressorException('Invalid load specified')
 
 
 def get_measure():
@@ -134,14 +145,24 @@ def send_results(stress_name, measures):
 def main():
     measures = []
 
-    stress = CPUStressor()
-
+    # Get initial power measure
     measures.append(get_measure())
-    for i in range(0, 50, 10):
-        stress.start(i)
-        sleep(5)
+
+    # Run CPU Stressor for 15 seconds
+    # By default, it runs at 100% of CPU load
+    stress = CPUStressor()
+    stress.run_for(15)
+
+    # Get current power measure
+    measures.append(get_measure())
+
+    # Now run a progressive CPU stressor
+    # from 10% to 50% of load
+    # Save power measure at each step
+    for load in range(10, 50, 10):
+        stress.load = load
+        stress.run_for(10)
         measures.append(get_measure())
-        stress.stop()
 
     send_results(stress.name, measures)
 
